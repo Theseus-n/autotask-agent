e7ae3c7fbfe3159c6dce222d72269430fdc957f3
Nehemy <nehemytangdiallaa@gmail.com>
2026-05-31 14:28:42 +0800

    Feat(agent): implement browser launch and calvin lms login workflow

--- agent.py
+++ agent.py
@@ -21,7 +21,7 @@ class LMSAgent:
     async def run_workflow(self):
         async with async_playwright() as p:
             print("Browser login..")
-            browser = await p.chromium.launch(headless=False)
+            browser = await p.chromium.launch(headless=False, slow_mo=2000)
             print("Browser make context..")
             context = await browser.new_context()
             print("Making page login..")
@@ -29,6 +29,18 @@ class LMSAgent:
 
             print(f"Logging in to {self.lms_url}")
             await page.goto(self.lms_url)
+
+            print("Logging account in...")
+            await page.fill("#username", self.username)
+            # Isi  Email 
+            await page.fill('#username', self.username)
+            # Isi Password
+            await page.fill('#password', self.password)
+            # Klik tombol Log in
+            await page.click('button:has-text("Log in")')
+
+            await page.wait_for_load_state("networkidle")
+            print("Berhasil masuk ke Dashboard LMS!")
 
             await asyncio.sleep(5)
             await browser.close()
